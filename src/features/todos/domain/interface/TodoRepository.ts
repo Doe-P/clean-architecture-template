c8577ee48808cda583b05055af1949b91f536528
doe <phoukhando@gmail.com>
2025-07-31 14:11:25 +0700

    Remove Todo-related interfaces, use cases, and network service implementation from the project.

--- src/features/todos/domain/interface/TodoRepository.ts
+++
@@ -1,7 +0,0 @@
-import { AppError } from "@/shared/errors/AppError";
-import { AsyncResult } from "@/shared/result/Result";
-import { ITodo } from "../entities/Todo";
-
-export interface TodoRepository {
-    getAll(): AsyncResult<AppError, ITodo[]>;
-}
